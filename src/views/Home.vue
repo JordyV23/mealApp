10f8889d74ab61e16b3fd476c3d08959b4aad9b3
JordyV23 <jordyvargas0123@gmail.com>
2023-01-26 15:28:45 -0600

    Inicio de proyecto

---
+++ src/views/Home.vue
@@ -0,0 +1,25 @@
+import { Text } from 'vue';
+
+<template>
+  <div class="flex flex-col p-8">
+    <input
+      type="text"
+      class="rounded border-2 border-gray-200 w-full"
+      placeholder="Search For Meals"
+    />
+
+    <div class="flex justify-center gap-1 mt-2">
+      <router-link to="/" v-for="letter of letters.split('')">
+        {{ letter }}</router-link
+      >
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { computed } from "vue";
+import store from "../store";
+
+const meals = computed(() => store.state.meals);
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+</script>
