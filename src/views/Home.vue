39174a764ee695e448a82456d5be36761f557713
JordyV23 <jordyvargas0123@gmail.com>
2023-01-28 18:02:56 -0600

    Busqueda de receta por nombre y mejora en la GUI

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,11 +2,7 @@ import { Text } from 'vue';
 
 <template>
   <div class="flex flex-col p-8">
-    <input
-      type="text"
-      class="rounded border-2 border-gray-200 w-full"
-      placeholder="Search For Meals"
-    />
+
 
     <div class="flex justify-center gap-1 mt-2">
       <router-link :to="{name:'byLetter',params:{letter}}" v-for="letter of letters.split('')" :key="letter">
